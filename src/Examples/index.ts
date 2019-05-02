54d56cf0a11a339926bd6478b7e1615467ba60cc
Micah Godbolt <micahgodbolt@gmail.com>
2019-05-02 13:46:45 -0700

    fixed bad export

--- src/Examples/index.ts
+++ src/Examples/index.ts
@@ -1,7 +1,6 @@
 export * from "./ChatInterface";
 export * from "./Example1";
 export * from "./MiniThemeDesigner";
-export * from "./SignUpSignInForm";
 export * from "./WeatherReportCard";
 export * from "./ControlsExample";
 export * from "./OneDriveUI";
